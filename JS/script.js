88a042b90d91969a57fd28b109a5729b9fda4f69
= <=>
2025-04-03 13:21:50 +0200

    fetch complete

--- JS/script.js
+++ JS/script.js
@@ -18,15 +18,15 @@ const getBooksFiltered = (filterType, filter) => {
         title = "Title: " + data.docs[i].title;
         author = "Authors: " + data.docs[i].author_name;
         publishYear = "First publish year: " + data.docs[i].first_publish_year;
-        genre = "";
         cover = `https://covers.openlibrary.org/b/olid/${data.docs[i].cover_edition_key}-M.jpg`;
+        console.log(title, author, publishYear, genre, cover);
       }
-      console.log(title, author, publishYear, genre, cover);
+
       console.log(data);
     });
 };
 
-const createBook = (title, author, year, genre, cover) => {};
+const createBook = (title, author, year, cover) => {};
 
 getBooksFiltered("author", "Rowling");
 
