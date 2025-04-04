88aec05796d64c96d695a82ec9ab57ba21948ba5
= <=>
2025-04-04 10:40:11 +0200

    clearList

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,12 @@
-let index = 0;
 const searchInput = document.getElementById("search_value");
 const searchBtn = document.getElementById("search_button");
 const booksContainer = document.getElementById("main_books");
-let currentFilter = "title";
 const selectedFilter = document.getElementById("search_select");
 
+let currentFilter = "title";
+
 const getBooksFiltered = (filterType, filter) => {
+  clearLastSearch();
   fetch(
     "https://openlibrary.org/search.json?" +
       filterType +
@@ -21,9 +22,8 @@ const getBooksFiltered = (filterType, filter) => {
           data.docs[i].author_name,
           data.docs[i].first_publish_year,
           `https://covers.openlibrary.org/b/olid/${data.docs[i].cover_edition_key}-M.jpg`,
-          index
+          i
         );
-        index++;
       }
     });
 };
@@ -48,6 +48,10 @@ const searchBook = () => {
 
 const getFilter = (event) => {
   currentFilter = event.target.value;
+};
+
+const clearLastSearch = () => {
+  booksContainer.innerHTML = "";
 };
 
 selectedFilter.addEventListener("change", getFilter);
